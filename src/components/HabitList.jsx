8152183582866f8b47e5eb04876086ad2e653d40
Guilherme Almeida <guilhermealmeidadaluz5@gmail.com>
2026-03-17 23:27:54 -0400

    feat(component): add validations and handler on HabitList component - exercise 5/7

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,9 +1,9 @@
 import HabitCard from "./HabitCard";
 import BemVindo from "./BemVindo";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function HabitList() {
-    
+    // useState:
     const [habits, setHabits] = useState( () => {
         // Esta função executa UMA VEZ - na montagem
         const stored = localStorage.getItem('my-daily-habits')
@@ -30,11 +30,52 @@ function HabitList() {
     const [novaCategoria, setNovaCategoria] = useState('')
     const [novaMeta, setNovaMeta] = useState(0)
 
+    const [erroNome, setErroNome] = useState('')
+    const [erroMeta, setErroMeta] = useState('')
+
+    // useRef:
+    const nomeInputRef = useRef(null)
+
     // useEffect:
+    // useEffect(() => {
+    //     const JsonStored = localStorage.getItem('my-daily-habits')
+    //     const ObjectStored = JSON.parse(JsonStored)
+
+    //     console.log("Elementos carregados do localStorage:", ObjectStored.length)
+    //     console.log("Elementos no Array inicial:", habits.length)
+    // }, [])
+
     useEffect( () => {
         localStorage.setItem('my-daily-habits', JSON.stringify(habits))
         document.title = `My Daily Habits - ${habits.length} hábito(s)`
     }, [habits])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        // [name] é uma chave dinâmica — usa o valor de name como nome da propriedade
+        if (name === 'novoNome') {
+            setNovoNome(value)
+            // Valida comprimento mínimo em tempo real
+            if (value.length > 0 && value.length < 3) {
+                setErroNome('O nome deve ter pelo menos 3 caracteres.')
+            } else {
+                setErroNome('')
+            }
+        }
+        if (name === 'novaDescricao') setNovaDescricao(value)
+        if (name === 'novaCategoria') setNovaCategoria(value)
+        if (name === 'novaMeta') {
+            const num = parseInt(value)
+            setNovaMeta(value)
+
+            if (num < 1 || num > 7) {
+                setErroMeta('Meta deve ser entre 1 e 7 dias.')
+            } else {
+                setErroMeta('')
+            }
+
+        }
+    }
 
     // Funções CRUD:
 
@@ -45,6 +86,17 @@ function HabitList() {
         if (!novoNome.trim()) {
             // se novoNome é campo vazio
             alert('Informe um nome para o hábito.')
+            return
+        }
+
+        // Bloqueia se há erro de validação
+        if (erroNome) {
+            nomeInputRef.current?.focus()
+            return
+        }
+
+        if (erroMeta) {
+            // nomeInputRef.current?.focus()
             return
         }
 
@@ -67,6 +119,9 @@ function HabitList() {
         setNovaDescricao('')
         setNovaCategoria('')
         setNovaMeta('')
+
+        // Devolve o foco para o campo nome - useRef em ação
+        nomeInputRef.current?.focus()
     }
     
     const removerHabit = (id) => {
@@ -76,18 +131,25 @@ function HabitList() {
         setHabits(habits.filter( habit => habit.id !== id ))
     }
 
-    const limparHistorico = () => {
+    const limparHistorico = (event) => {
+        console.log(event)
         localStorage.removeItem('my-daily-habits')
         setHabits([
-
+            { id: 1, nome: 'Exercício', descricao: 'Treino de força', meta: 5, ativo: true, diasFeitos: 5 },
+            { id: 2, nome: 'Leitura', descricao: 'Livro ou artigo', meta: 7, ativo: true, diasFeitos: 3 },
+            { id: 3, nome: 'Meditação', descricao: 'Respiração e foco', meta: 7, ativo: false, diasFeitos: 0 },
+            { id: 4, nome: 'Hidratação', descricao: 'Beber 2L de Água', meta: 7, ativo: true, diasFeitos: 6 }
         ])
     }
+
+    const QuantidadeHabitosAtivos = habits.filter(h => h.ativo).length
 
     // retorno padrão estilo switch case quando as condições acima não são satisfeitas no fluxo do programa
     return (
         <>
             <div className='bem-vindo'>
                 <BemVindo nomeUsuario="turma iteam" totalHabitos={habits.length}/>
+                <p>{`Hábitos ativos: ${QuantidadeHabitosAtivos}`}</p>
             </div>
             <form onSubmit={adicionarHabit} className="habit-form">
                 <div>
@@ -95,19 +157,23 @@ function HabitList() {
                         Nome do hábito <span style={{color: "red", fontWeight: "bold"}}>*</span>
                         <input
                             type="text"
+                            name="novoNome"
                             value={novoNome}
-                            onChange={ (e) => setNovoNome(e.target.value) }
+                            onChange={ handleChange }
+                            ref={nomeInputRef}
                             placeholder="Digite aqui o seu hábito"
                         />
                     </label>
+                    {erroNome && <p style={{ color: 'red', fontSize: '0.8rem'}}>{erroNome}</p>}
                 </div>
                 <div>
                     <label>
                         Descrição
                         <input 
                             type="text"
+                            name="novaDescricao"
                             value={novaDescricao}
-                            onChange={ (e) => setNovaDescricao(e.target.value) }
+                            onChange={ handleChange }
                             placeholder="Digite aqui a descrição do seu hábito"
                         />
                     </label>
@@ -117,8 +183,9 @@ function HabitList() {
                         Categoria
                         <input 
                             type="text"
+                            name="novaCategoria"
                             value={novaCategoria}
-                            onChange={ (e) => setNovaCategoria(e.target.value) }
+                            onChange={ handleChange }
                             placeholder="Digite a categoria que ele se encaixa"
                         />
                     </label>
@@ -128,16 +195,18 @@ function HabitList() {
                         Meta
                         <input 
                             type="number"
+                            name="novaMeta"
                             value={novaMeta}
-                            onChange={ (e) => setNovaMeta( parseInt(e.target.value) ) }
+                            onChange={ handleChange }
                             min={0}
                             placeholder="Digite o número de dias que o hábito deve ocorrer"
                         />
                     </label>
+                    {erroMeta && <p style={{ color: 'red', fontSize: '0.8rem' }}>{erroMeta}</p>}
                 </div>
                 <div className="buttons-container">
                     <button type="submit"> <span>✍</span> Adicionar hábito</button>
-                    <button onClick={limparHistorico}> <span>🧹</span> Limpar histórico</button>
+                    <button type="button" onClick={limparHistorico}> <span>🧹</span> Limpar histórico</button>
                 </div>
             </form>
             <ul style={{margin: 0, padding: 0}}>
